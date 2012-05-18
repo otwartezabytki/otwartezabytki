b2e089ff5262cd5d685f24f2c685fbfa531de08d
chytreg <dariusz.gertych@gmail.com>
2012-05-18 16:41:12 +0200

    rails 3.2 and basic settings

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Otwartezabytki::Application.config.secret_token = 'a689cbc3f8319939eeabc992e3210c64e7b06dc5b005d09e2cb5fbe28a450c69c77198150b0b38512fed5981dd3cac4d03cf4d809021edb6d6f5455c87b375f0'
---
+++ spec/spec_helper.rb
@@ -0,0 +1,24 @@
+require 'rubygems'
+require 'spork'
+
+Spork.prefork do
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+  RSpec.configure do |config|
+    config.mock_with :rspec
+    config.fixture_path = "#{::Rails.root}/spec/fixtures"
+    config.use_transactional_fixtures = true
+
+    # Needed for Spork
+    ActiveSupport::Dependencies.clear
+  end
+end
+
+Spork.each_run do
+  load "#{Rails.root}/config/routes.rb"
+  Dir["#{Rails.root}/app/**/*.rb"].each { |f| load f }
+end
