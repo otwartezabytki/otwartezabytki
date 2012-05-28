802abc0d84f6ea84ef1403c6dca0427196be7daa
chytreg <dariusz.gertych@gmail.com>
2012-05-28 10:20:32 +0200

    sort relics by id ASC

--- app/controllers/relics_controller.rb
+++ app/controllers/relics_controller.rb
@@ -1,5 +1,5 @@
 # -*- encoding : utf-8 -*-
 class RelicsController < ApplicationController
-  expose(:relics) { Relic.page(params[:page]) }
+  expose(:relics) { Relic.order('id ASC').page(params[:page]) }
   expose(:relic)
 end
