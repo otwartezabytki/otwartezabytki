eaabcbbf6b8399274f3f574be0bed52a8ba1c5b3
chytreg <dariusz.gertych@gmail.com>
2012-06-05 15:03:18 +0200

    sort facets navigators in alphabetical order

--- app/controllers/relics_controller.rb
+++ app/controllers/relics_controller.rb
@@ -2,6 +2,7 @@
 class RelicsController < ApplicationController
   expose(:relics) { Relic.search(params) }
   expose(:relic)
+  helper_method :navigators
 
   def update
     if relic.update_attributes(params[:relic])
@@ -10,4 +11,22 @@ class RelicsController < ApplicationController
       render :edit
     end
   end
+
+  def navigators
+    navigators = {}
+    ['voivodeships', 'communes', 'districts', 'places'].each do |name|
+      next unless relics.facets[name]
+      ids = relics.facets[name]['terms'].map { |k| k['term']}
+      klass = name.classify.constantize
+      objs = klass.where(:id => ids).order('id ASC')
+      sorted_counts = relics.facets[name]['terms'].sort_by { |k| k['term'].to_i }.map { |k| k['count'] }
+      objs.each_with_index do |o, i|
+        o.class_eval "attr_accessor :count"
+        o.count = sorted_counts[i]
+      end
+      navigators[name] = objs.sort_by { |k| k.name.parameterize }
+    end if relics and relics.facets
+    navigators
+  end
+
 end
