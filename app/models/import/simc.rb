730626de311470b26a24bd240142f54e12fea00e
chytreg <dariusz.gertych@gmail.com>
2012-06-13 12:56:27 +0200

    import simc structure

--- app/models/import/simc.rb
+++ app/models/import/simc.rb
@@ -12,9 +12,21 @@ module Import
     property :rm, String
     property :mz, String
     property :nazwa, String
-    property :sym, String
+    property :sym, String, :key => true
     property :sympod, String
     property :stan_na, String
+
+    class << self
+      def import_all!
+        Simc.all.batch(1000) do |t|
+          c = Commune.joins(:district => :voivodeship).where(
+            'voivodeships.nr' => t.woj, 'districts.nr' => t.pow, 'communes.nr' => t.gmi, 'communes.kind' => t.rodz_gmi
+          ).first
+          conds = {:name => Unicode.capitalize(t.nazwa), :sym => t.sym}
+          c.places.where(conds).first || c.places.create(conds)
+        end
+      end
+    end
 
   end
 end
