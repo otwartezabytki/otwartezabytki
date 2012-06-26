e681020a2e6ee63f5e4e0cb2bd73a27bbdc9fca9
chytreg <dariusz.gertych@gmail.com>
2012-06-26 16:22:52 +0200

    high voltage static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,17 @@
 # -*- encoding : utf-8 -*-
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  before_filter do
-    I18n.locale = :pl
-  end
+  helper_method :page_pl_path
 
   def current_user!
     unless user_signed_in?
       sign_in User.create!
     end
-   
+
     current_user
+  end
+
+  def page_pl_path(path)
+    "/strony/#{path}"
   end
 end
