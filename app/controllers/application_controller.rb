a2040efc9a961646ee49e557c9a5e7285ad06276
chytreg <dariusz.gertych@gmail.com>
2012-06-26 18:10:46 +0200

    do not load rails objects on search action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 # -*- encoding : utf-8 -*-
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter do
+    I18n.locale = I18n.default_locale
+  end
   helper_method :page_pl_path
 
   def current_user!
