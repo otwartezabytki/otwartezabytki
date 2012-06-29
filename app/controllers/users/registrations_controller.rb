1913f30ca73148231deef99666013300bebd6d10
Adam Stankiewicz <sheerun@sher.pl>
2012-06-29 16:17:09 +0200

    welcome e-mail updated

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -19,6 +19,7 @@ class Users::RegistrationsController < ApplicationController
     if current_user.update_attributes(params[:user])
       sign_in current_user, :bypass => true
       flash[:notice] = t('notices.reservation_thanks')
+      UserMailer.welcome_email(current_user, current_user.password).deliver
       respond_with current_user, :location => after_update_path_for(current_user)
     else
       respond_with current_user
