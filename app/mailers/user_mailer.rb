03a0ba199f7f7607b2fc9318aa82166089351592
Adam Stankiewicz <sheerun@sher.pl>
2012-06-29 17:01:59 +0200

    password recovery in welcome e-mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,10 @@
 class UserMailer < ActionMailer::Base
   default from: "no-reply@otwartezabytki.pl"
 
-  def welcome_email(user, password)
+  def welcome_email(user, password, reset_password_token)
     @user = user
     @password = password
+    @reset_password_token = reset_password_token
 
     mail(:to => user.email, :subject => "Witaj w Otwartych Zabytkach!")
   end
