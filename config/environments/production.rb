92e7d0b0aa2413c742af3614313ad8f5bbad7c74
chytreg <dariusz.gertych@gmail.com>
2012-06-29 17:54:35 +0200

    :sendmail na staging i production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,7 @@ Otwartezabytki::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :sendmail
   config.action_mailer.default_url_options = { :host => "otwartezabytki.pl" }
 
   # Enable threaded mode
