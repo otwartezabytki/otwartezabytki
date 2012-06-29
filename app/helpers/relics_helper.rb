13d64dedbdcdbb54aa1e5f3046b2a1cf7a265bfd
Adam Stankiewicz <sheerun@sher.pl>
2012-06-29 13:13:24 +0200

    commune and district names not shown when descendants are placed in different ones

--- app/helpers/relics_helper.rb
+++ app/helpers/relics_helper.rb
@@ -1,9 +1,5 @@
 # -*- encoding : utf-8 -*-
 module RelicsHelper
-  def wizard_full_place(relic)
-    ["woj. #{relic.voivodeship.name}", "pow. #{relic.district.name}", "gm. #{relic.commune.name}"].join(', ')
-  end
-
   def thank_you_note
     [
       "Dzięki Tobie Cyfrowy Czyn Społeczny działa :)",
