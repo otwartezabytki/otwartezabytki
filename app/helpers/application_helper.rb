e6c2fd98715e96c45f9ede36a780e1b1b46436d3
Tymon Tobolski <i@teamon.eu>
2012-07-02 23:41:53 +0200

    No results view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -49,24 +49,26 @@ module ApplicationHelper
 
     first = types.shift
     count = Relic.search(:q1 => first).total_count
-    suggestions[0] = link_to "#{first} (#{count})", relics_path(:q1 => first)
+    suggestions[0] = [first, count]
 
     first = places.shift
     count = Relic.search(:q1 => first).total_count
-    suggestions[2] = link_to "#{first} (#{count})", relics_path(:q1 => first)
+    suggestions[2] = [first, count]
 
     types.each do |type|
       places.each do |place|
         q = type + " " + place
         count = Relic.search(:q1 => q).total_count
         if count > 0
-          suggestions[1] = link_to "#{q} (#{count})", relics_path(:q1 => q)
+          suggestions[1] = [q, count]
           break
         end
       end
     end
 
-    suggestions.join(", ").html_safe
+    suggestions.map do |label, count|
+      link_to "#{label} <span>(#{count})</span>".html_safe, relics_path(:q1 => label)
+    end.join(", ").html_safe
   end
 
 	def link_to_facet obj, location, deep, &block
