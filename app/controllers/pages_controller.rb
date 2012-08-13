0f4b8f1865fa82b6f3a93b2e22b9890c529a202e
chytreg <dariusz.gertych@gmail.com>
2012-08-13 10:49:45 +0200

    export db download url

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,15 +3,27 @@ class PagesController < HighVoltage::PagesController
 
   layout :layout_for_page
 
-  protected
-  def layout_for_page
-    case params[:id]
-      when 'share_close'
-        nil
-      when 'under_construction'
-        'under_construction'
-      else
-        'application'
+  def show
+    file_path = Rails.root.join('public', 'system', 'relics_history.csv')
+    if File.exists?(file_path)
+      @export_url = '/system/relics_history.csv'
+      @export_date = File.atime(file_path)
+      @export_size = (File.size(file_path) / 1024.0 / 1024.0).round(2)
     end
+    super
   end
+
+  protected
+    def layout_for_page
+      case params[:id]
+        when 'share_close'
+          nil
+        when 'under_construction'
+          'under_construction'
+        else
+          'application'
+      end
+    end
+
+
 end
