e9910ac76b078adf47faab25fac0c37adc70f532
Adam Stankiewicz <sheerun@sher.pl>
2012-08-13 16:30:35 +0200

    separate general section into title + tags and categories

--- config/initializers/formtastic.rb
+++ config/initializers/formtastic.rb
@@ -1,1 +1,19 @@
 Formtastic::Helpers::FormHelper.builder = FormtasticBootstrap::FormBuilder
+FormtasticBootstrap::FormBuilder.i18n_lookups_by_default = true
+FormtasticBootstrap::FormBuilder.default_inline_hint_class = :block
+
+module FormtasticBootstrap
+  module Inputs
+    module Base
+      module Wrapping
+        # Override this method if you want to change the display order (for example, rendering the
+        # errors before the body of the input).
+        def input_div_wrapping(inline_or_block_errors = :inline)
+          template.content_tag(:div, :class => "input") do
+            [hint_html(inline_or_block_errors), yield, error_html(inline_or_block_errors)].join("\n").html_safe
+          end
+        end
+      end
+    end
+  end
+end
