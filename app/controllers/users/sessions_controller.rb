73f532ffb8dfac25b6e6b63e9ed0a94d795a5f1b
Adam Stankiewicz <sheerun@sher.pl>
2012-10-18 17:14:08 +0200

    fix: remove notice flash messages on login/logout

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,19 +3,25 @@ class Users::SessionsController < Devise::SessionsController
 
   before_filter :enable_fancybox
 
-  # GET /resource/sign_in
-  def new
-    resource = build_resource(nil, :unsafe => true)
-    clean_up_passwords(resource)
-    respond_with(resource, serialize_options(resource))
-  end
-
   # POST /resource/sign_in
   def create
-    resource = warden.authenticate!(auth_options)
-    set_flash_message(:notice, :signed_in) if is_navigational_format?
+    self.resource = warden.authenticate!(auth_options)
     sign_in(resource_name, resource)
     respond_with resource, :location => after_sign_in_path_for(resource)
+  end
+
+  # DELETE /resource/sign_out
+  def destroy
+    redirect_path = after_sign_out_path_for(resource_name)
+
+    # We actually need to hardcode this as Rails default responder doesn't
+    # support returning empty response on GET request
+    respond_to do |format|
+      format.any(*navigational_formats) { redirect_to redirect_path }
+      format.all do
+        head :no_content
+      end
+    end
   end
 
 end
