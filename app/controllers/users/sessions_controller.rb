397782195130fa807368fd2547a5f05a07d29d3e
Adam Stankiewicz <sheerun@sher.pl>
2012-10-19 12:39:43 +0200

    fix: logout

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -13,6 +13,7 @@ class Users::SessionsController < Devise::SessionsController
   # DELETE /resource/sign_out
   def destroy
     redirect_path = after_sign_out_path_for(resource_name)
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
 
     # We actually need to hardcode this as Rails default responder doesn't
     # support returning empty response on GET request
