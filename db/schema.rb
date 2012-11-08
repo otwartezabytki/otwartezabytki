015f51a531e12f1d9842540758881e63e1a83d02
chytreg <dariusz.gertych@gmail.com>
2012-11-08 16:44:28 +0100

    change alert description type

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121025095542) do
+ActiveRecord::Schema.define(:version => 20121108154129) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(:version => 20121025095542) do
   create_table "alerts", :force => true do |t|
     t.integer  "relic_id"
     t.integer  "user_id"
-    t.string   "description"
+    t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.string   "file"
