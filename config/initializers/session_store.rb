441ac7dc70388512b40b465b782f07a2dc8dd804
chytreg <dariusz.gertych@gmail.com>
2012-11-23 17:12:46 +0100

    fix session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 # Be sure to restart your server when you modify this file.
 
-Otwartezabytki::Application.config.session_store :cookie_store, key: '_otwartezabytki_session', domain: Settings.oz_host
+Otwartezabytki::Application.config.session_store :cookie_store, key: '_oz_session', domain: Settings.oz.host
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
