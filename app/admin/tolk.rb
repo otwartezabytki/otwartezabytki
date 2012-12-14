9d452de9621f265656b27b8ba7dcc2c261dda54f
Dariusz Gertych <dariusz.gertych@gmail.com>
2012-12-14 17:51:07 +0100

    Tolk config add missing locales

--- app/admin/tolk.rb
+++ app/admin/tolk.rb
@@ -3,3 +3,8 @@
 ActiveAdmin.register_page "Tolk" do
   menu :label => 'Tłumaczenia', :url => '/admin/tolk'
 end
+Tolk.config do |config|
+  config.mapping["by"] = 'Belarus'
+  config.mapping["se"] = 'Spanish'
+  config.mapping["ua"] = 'Ukrainian'
+end
