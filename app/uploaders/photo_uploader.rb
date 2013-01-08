c18c564f68e16c3cb7faca8a2f05a76c43b3e461
Dariusz Gertych <dariusz.gertych@gmail.com>
2013-01-08 12:21:58 +0100

    Revert "fixed default url for fb thumbs"
    This reverts commit a4aa63ff82a401c35b5aaf194656ffc475618c4d.

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -50,6 +50,6 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # fallback
   def default_url
-    "assets/fallback/" + ["photo", version_name, "default.png"].compact.join('_')
+    "fallback/" + ["photo", version_name, "default.png"].compact.join('_')
   end
 end
