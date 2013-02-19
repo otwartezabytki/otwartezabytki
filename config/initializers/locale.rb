0c3b03d7cfd26f3f1497512d9c1a74671514c021
Dariusz Gertych <dariusz.gertych@gmail.com>
2013-02-19 22:46:51 +0100

    Move afte_save hack for locale to tolk gem

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -11,15 +11,6 @@ I18n::Backend::ActiveRecord.send(:include, I18n::Backend::Cache)
 I18n.cache_store = ActiveSupport::Cache.lookup_store(:dalli_store, { :namespace => "oz-i18n-#{Rails.env}", :expires_in => 1.month, :compress => true })
 I18n.backend = I18n::Backend::Chain.new(I18n::Backend::ActiveRecord.new, I18n.backend)
 
-# clear cache after transaltaion update
-Tolk::Translation.instance_eval do
-  after_save do
-    # do not remove logger line !!!
-    Rails.logger.error "clean i18n cache store"
-    I18n.cache_store.clear
-  end
-end
-
 # define I18n collator method
 module I18n
   class << self
