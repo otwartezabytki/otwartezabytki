1d07557bf7c9b6b371ca4c6073ba309529cb9bd4
Dariusz Gertych <dariusz.gertych@gmail.com>
2014-03-12 15:50:44 +0100

    Remove unused code from application_helper

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -4,36 +4,6 @@ require 'spec_helper'
 describe ApplicationHelper do
   before(:each) { refresh_relics_index }
 
-  context "users statistics" do
-    it "should return statistic results" do
-      users_statistics.is_a? Array
-      users_statistics.should have(3).items
-      users_statistics.all? &:present?
-    end
-  end
-
-  context "users_activity_statistics" do
-    it "should return statistic results" do
-      users_activity_statistics.is_a? Array
-      users_activity_statistics.should have(2).items
-    end
-  end
-
-  context "relics_statistics" do
-    it "should return statistic results" do
-      relics_statistics.is_a? Array
-      relics_statistics.should have(4).items
-    end
-  end
-
-  context "random_search_suggestions" do
-    it "should return string of links" do
-      random_search_suggestions.is_a? String
-      random_search_suggestions.split(', ').should have(3).items
-      random_search_suggestions.should match /(<a href="\/relics">.*?<span>.*?<\/span><\/a>)/
-    end
-  end
-
   context "link_to_facet" do
     before do
       def search_params; {}; end
