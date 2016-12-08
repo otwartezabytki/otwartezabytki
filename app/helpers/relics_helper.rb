e91a1514bca8ab0297a34fba7d67d0023d979958
Fryderyk Markiewicz <fmarkiewicz@netkodo.com>
2016-12-08 11:50:29 +0100

    alt in img tags

--- app/helpers/relics_helper.rb
+++ app/helpers/relics_helper.rb
@@ -28,7 +28,7 @@ module RelicsHelper
 
   def subrelic_image(relic)
     content_tag :dd, class: 'subrelic-image' do
-      image_tag(relic.main_photo.file.url(:icon), :size => '24x24', :class => 'thumb type-tiny', :alt => relic.identification)
+      image_tag(relic.main_photo.file.url(:icon), :size => '24x24', :class => 'thumb type-tiny', :alt => '')
     end
   end
 
